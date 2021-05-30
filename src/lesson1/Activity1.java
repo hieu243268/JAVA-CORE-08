b6d9c9d1c552cc1653f3ef8cc6e5ffd280815c87
= <hieutmhe151017@fpt.edu.vn>
2021-05-30 17:17:46 +0700

    update code

--- src/lesson1/Activity1.java
+++ src/lesson1/Activity1.java
@@ -1,8 +1,8 @@
-package BAITAP.BAI1;
+package lesson1;
 
 import java.util.Scanner;
 
-public class bai1 {
+public class Activity1 {
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             System.out.print("Input first number: ");
