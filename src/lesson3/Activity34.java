c93ed498befc86fc899fa558c45872b4e8fa5e35
= <hieutmhe151017@fpt.edu.vn>
2021-06-01 22:34:53 +0700

    Activity34

--- src/lesson3/Activity34.java
+++ src/lesson3/Activity34.java
@@ -4,14 +4,13 @@ public class Activity34 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input number: ");
-        int n = sc.nextInt();
-        int sum = 0, a;
+        double n = sc.nextDouble();
+        double sum = 0;
         for (int i = 0; i <= n; i++) {
             sum += i;
-
         }
-        System.out.println("The sum of 1 to" + n + "is" + sum);
-       // System.out.println("The average is" + a);
+        System.out.println("The sum of 1 to " + n + " is " + sum);
+        System.out.println("The average is " +  (sum/n));
     }
 
 }
